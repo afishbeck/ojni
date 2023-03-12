b5d22fbc8e9f35aa00a2589590012409464ed5b5
Anthony Fishbeck <anthony.fishbeck@lexisnexis.com>
2023-03-12 03:03:54 -0400

    w

--- processor.go
+++ processor.go
@@ -128,8 +128,8 @@ func (ltp *logsJniProcessor) ConsumeLogs(ctx context.Context, ld plog.Logs) erro
 	nenv := jvm.AttachCurrentThread()
 
 	var processedBytes []byte
-	if err = nenv.CallStaticMethod("OtelJniProcessor", "processLogs", &processedBytes, marshaledBytes); err != nil {
-		ltp.logger.Info("error callning Java method OtelJniProcessor/Processor.processLogs")
+	if err = nenv.CallStaticMethod("ojnilib.Library", "processLogs", &processedBytes, marshaledBytes); err != nil {
+		ltp.logger.Info("error callning Java method ojnilib.Library.processLogs")
 		ltp.logger.Error(err.Error())
 		return nil
 	}
@@ -204,8 +204,8 @@ func (ltp *tracesJniProcessor) ConsumeTraces(ctx context.Context, traces ptrace.
 	nenv := jvm.AttachCurrentThread()
 
 	var processedBytes []byte
-	if err = nenv.CallStaticMethod("OtelJniProcessor", "processTraces", &processedBytes, marshaledBytes); err != nil {
-		ltp.logger.Info("error callning Java method OtelJniProcessor/Processor.processTraces")
+	if err = nenv.CallStaticMethod("ojnilib.Library", "processTraces", &processedBytes, marshaledBytes); err != nil {
+		ltp.logger.Info("error callning Java method ojnilib.Library.processTraces")
 		ltp.logger.Error(err.Error())
 		return nil
 	}
@@ -281,8 +281,8 @@ func (ltp *metricsJniProcessor) ConsumeMetrics(ctx context.Context, metrics pmet
 	nenv := jvm.AttachCurrentThread()
 
 	var processedBytes []byte
-	if err = nenv.CallStaticMethod("OtelJniProcessor", "processMetrics", &processedBytes, marshaledBytes); err != nil {
-		ltp.logger.Info("error callning Java method OtelJniProcessor/Processor.processMetrics")
+	if err = nenv.CallStaticMethod("ojnilib.Library", "processMetrics", &processedBytes, marshaledBytes); err != nil {
+		ltp.logger.Info("error callning Java method ojnilib.Library.processMetrics")
 		ltp.logger.Error(err.Error())
 		return nil
 	}
